85deafdf093e81b60271bf90fb6e23193eac9d44
DanielCreations08 <fminnovations.designs@gmail.com>
2024-01-15 13:30:56 -0500

    Nav bar start

---
+++ script.js
@@ -0,0 +1,11 @@
+// Nav Bar Start
+    function toggleNavbar() {
+        var x = document.getElementById("myNavbar");
+        if (x.className === "navbar") {
+            x.className += " responsive";
+        } else {
+            x.className = "navbar";
+        }
+    }
+
+ // Nav Bar Ends   
